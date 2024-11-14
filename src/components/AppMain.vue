b9e55ab0d9d1dd8dc00b6814f8fc2a058a8d48cd
FrancescoMinichiello <fraps2278@gmail.com>
2024-11-14 19:00:30 +0100

    Added cards in app main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,21 +5,54 @@ export default{
     name: "AppMain",
     data(){
         return{
-
+            projectList:[],
         }
     },
     methods:{
         getProjects(){
-
+            axios.get('http://127.0.0.1:8000/api/projects')
+                .then((response) => {
+                   this.projectList = response.data.results
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
         }
     },
+    created(){
+       this.getProjects();
+    }
 
 }
 </script>
 
 <template>
-
-
+<main class="container">
+    <div class="row py-5">
+        <div class="col-12 mb-3">
+            <h2>
+                Projects List:
+            </h2>
+        </div>
+        <div class="col">
+            <article class="card" v-for="singleProject in projectList" :key="singleProject.id">
+                <div class="card-body">
+                    <h1 class="card-title">
+                        {{ singleProject.name }}
+                    </h1>
+                    <div class="card-subtitle" v-for="technology in singleProject.technologies" :key="technology.id">
+                        <h3>
+                          tecnologie usate:  {{ technology.name }}
+                        </h3>
+                    </div>
+                    <div class="card-text">
+                       <a :href="singleProject.link">Vai al progetto</a> 
+                    </div>
+                </div>
+            </article>
+        </div>
+    </div>
+</main>
 </template>
 
 <style>
